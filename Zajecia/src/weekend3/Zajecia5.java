c92426e971a3b1a5d70e176cac5f9d1c194a3b8f
HubZdalLip <karabe181818@gmail.com>
2020-08-08 11:33:04 +0200

    zaj 5 commit zajeciowy 11:32

--- Zajecia/src/weekend3/Zajecia5.java
+++ Zajecia/src/weekend3/Zajecia5.java
@@ -23,20 +23,61 @@ public class Zajecia5 {
         // typ zwracany nie ma znaczenia....
         // kompilator jest w stanie rozroznic argumenty ktore sa przekazywane....
 
-        double sumD = add(5.1, 10.9);
-        System.out.println(sumD);
-        System.out.println(add(5.13, 5));
-        System.out.println(add(5, "10.1"));
+//        double sumD = add(5.1, 10.9);
+//        System.out.println(sumD);
+//        System.out.println(add(5.13, 5));
+//        System.out.println(add(5, "10"));
 
         // zadanie napisz metode welcomeUser
 //         a) bedzie przyjmowac imie i zwracac komunikat witajacy
 //         b) bedzie przyjmowac imie i wiek i zwracac podstawowe ifnormacje
 //         c) bedzie przyjmowac imie, wiek i nazwisko i zwracac te informacje
 
+        String result = welcomeUser("Hubert");              //a
+        System.out.println(result);
+        result = welcomeUser("Natalia");
+        System.out.println(result);
+        result = welcomeUser("Hubert", 40);             //b
+        System.out.println(result);
+        result = welcomeUser("Władek", 15);
+        System.out.println(result);
+        result = welcomeUser("Hubert", 40, "Nowak");             //b
+        System.out.println(result);
+        result = welcomeUser("Magda");
+        System.out.println(result);
+        result = welcomeUser("Władek", 15);
+        System.out.println(result);
+        result = welcomeUser("Pawel", 15, "Kowalski");
+        System.out.println(result);
+
+        // uwaga! przeciazenie metody to nic specjalnego.... na etapie kompilowania program dopisuje do nazw metod kilka liter ktore pozwalaja mu rozroznic te metody
+
         // zadanie. Napisz metode ktora przyjmie nieokreslona liczbe elementow int oraz jeden element typu String zaweirajacy liczbe. Zwroc roznice....
 
-
+        System.out.println("Roznica to : " + subEx("7", 5, 6, 1, 8));
     }
+
+    private static int subEx(String s, int... ints) {
+        int numberFromString = Integer.parseInt(s); // parsowanie wartosci string na int w tym miejscu
+        int init = numberFromString; // wartosc poczatkowa ma to sparsowana liczba
+        for (int value : ints) {
+            init -= value;
+        }
+        return init;
+    }
+
+    private static String welcomeUser(String name) {
+        return "Mam na imie " + name;
+    }
+
+    private static String welcomeUser(String name, int age) {
+        return "Mam na imie " + name + ". Metoda jest przeciazona. Moj wiek to " + age + " lat";
+    }
+
+    private static String welcomeUser(String name, int age, String surname) {
+        return "Mam na imie " + name + ". Moje nazwisko to " + surname + ". Metoda jest przeciazona \"jeszcze bardziej\". Moj wiek to " + age + " lat";
+    }
+
 
     private static double add(int i, String string) {
         int number = Integer.parseInt(string);          //istnieje rowniez parseDouble.....
